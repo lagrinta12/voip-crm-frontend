1c1bafb584dd5f57e6df708a335e19294dcd06ef
lagrinta12 <victorvergon55@gmail.com>
2026-03-22 20:11:10 +0100

    fix: remove docker-entrypoint.sh to use nixpacks instead

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Railway injecte la variable PORT dynamiquement
-# On remplace le placeholder dans la config nginx
-PORT=${PORT:-80}
-sed -i "s/PORT_PLACEHOLDER/$PORT/g" /etc/nginx/conf.d/default.conf
-
-exec "$@"
